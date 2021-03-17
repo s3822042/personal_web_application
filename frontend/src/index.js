5699ab1c8f831e15b57ab4ba46033f7b393aa6f9
Luan Vo Thanh <s3822042@rmit.edu.vn>
2021-03-17 20:46:36 +0700

    add webpack config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,9 +9,10 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  <App title={title} />,
   document.getElementById('root'),
 )
-
+module.hot.accept()
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
---
+++ frontend/webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/start.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+    hot: true,
+  },
+}

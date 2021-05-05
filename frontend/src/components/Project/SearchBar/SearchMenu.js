48ee9e31f61c211990f3d87f17e9f06d25fa2580
s3822042 <s3822042@rmit.edu.vn>
2021-05-05 16:55:00 +0700

    update correct import

--- frontend/src/components/Project/SearchBar/SearchMenu.js
+++ frontend/src/components/Project/SearchBar/SearchMenu.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./SearchMenu.css";
-import SearchIcon from "./SearchIcons";
+import SearchIcons from "./SearchIcons";
 
 function SearchMenu() {
   return (
-    <div className="pt-5 px-2">
+    <div className="px-2">
       <div className="search_bar w-full mx-auto flex h-12 items-center">
         <div className="flex w-5 items-center">
-          <SearchIcon />
+          <SearchIcons />
         </div>
         <input type="text" />
       </div>

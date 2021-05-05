fc31969a69235a605bacb854d25b9bfdb4224e1a
s3822042 <s3822042@rmit.edu.vn>
2021-05-05 16:55:31 +0700

    new component added

---
+++ frontend/src/components/Experience/Experience.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Experience() {
+  return <div>HEllo</div>;
+}
+
+export default Experience;
--- frontend/src/views/ExperienceView.js
+++ frontend/src/views/ExperienceView.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import NavBar from "../components/NavBar/NavBar";
+import Experience from "../components/Experience/Experience";
 import Footer from "../components/Footer/Footer";
 
 const ExperienceView = () => (
   <div>
     <NavBar />
+    <Experience />
     <Footer />
   </div>
 );
